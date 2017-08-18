ec9848fe0d1c1d1ba7098db7b183ecb4714de9e3
David Pickart <davidstoreypickart@gmail.com>
2017-08-18 14:20:36 -0500

    Remove dependency on lp-utils

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+export { default as capitalize } from 'lodash/capitalize'
+export { default as lowerCase } from 'lodash/lowerCase'
+export { default as mapValues } from 'lodash/mapValues'
+
+export { default as createFilterFunction } from './create-filter-function'
+export { default as flatToNested } from './flat-to-nested'
