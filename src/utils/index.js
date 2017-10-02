873e203636a372974962d61fe022c6c29b35cf9e
David Pickart <davidstoreypickart@gmail.com>
2017-10-02 14:27:23 -0500

    Curry validate function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
+// Lodash
 export { default as capitalize } from 'lodash/capitalize'
+export { default as curry } from 'lodash/curry'
 export { default as lowerCase } from 'lodash/lowerCase'
 export { default as mapValues } from 'lodash/mapValues'
 
+// Local
 export { default as createFilterFunction } from './create-filter-function'
 export { default as flatToNested } from './flat-to-nested'
+export { default as submitWithFilter } from './submit-with-filter'
