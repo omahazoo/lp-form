9fb0942407a6913d1e58a4190259b14846209b11
David Pickart <davidstoreypickart@gmail.com>
2017-12-12 12:05:50 -0600

    Rename wrappedOnChange

--- src/utils/create-submitting-on-change.js
+++ src/utils/create-submitting-on-change.js
@@ -2,7 +2,7 @@
 // Creates an onChange function that calls an existing onChange function, then submits the form.
 
 function createSubmittingOnChange(onChange) {
-    return function (params, dispatch, props, ...rest) {
+    return function wrappedOnChange (params, dispatch, props, ...rest) {
         onChange(params, dispatch, props, ...rest)
         return props.submit()
     }
