0262efe12342bb3602ebc4498a327e1d9cd4791c
Conor Hawes <conor@launchpadlab.com>
2019-09-13 13:37:39 -0500

    Enhance robustness of error mapping

--- src/middleware/wrapSubmissionPromise.js
+++ src/middleware/wrapSubmissionPromise.js
@@ -1,7 +1,7 @@
 import { withPropsOnChange } from 'recompose'
 import { SubmissionError } from 'redux-form'
 import isPromise from 'is-promise'
-import { getOr } from 'lodash/fp'
+import { get } from 'lodash/fp'
 
 // Wrap submission results in a redux-form SubmissionError. 
 // Also ensures that the return value of onSubmit is a promise.
@@ -12,17 +12,30 @@ const wrapSubmissionPromise = withPropsOnChange(
       onSubmit: (...args) => {
         const result = onSubmit(...args)
         if (!isPromise(result)) return Promise.resolve(result)
-        return result.catch(err => {
-          const messages = getOr({}, 'errors', err)
-          const submissionError = new SubmissionError(messages)
-          
-          // Retain metadata (e.g., status code) about the original error
-          submissionError.meta = { error: err }
-          throw submissionError
-        })
+        return result.catch(mapSubmissionError)
       }
     }
   }
 )
+
+function mapSubmissionError (error) {
+  const messages = getErrorMessages(error)
+  const submissionError = new SubmissionError(messages)
+  
+  // Retain metadata (e.g., status code) about the original error
+  submissionError.meta = { error }
+  throw submissionError
+}
+
+// Checks for an error that matches LPL's default standard for mapping error messages (HttpError).
+// Else defaults to the standard error API and maps to a redux-form "form-wide" error key.
+function getErrorMessages (err) {
+  const messages = get('errors', err)
+  
+  if (messages) return messages
+  
+  const _error = get('message', err)
+  return _error ? { _error } : {}
+}
 
 export default wrapSubmissionPromise
